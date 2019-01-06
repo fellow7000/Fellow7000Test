e7765d67019ad8e6ca384c856fa2edb54c0ecf65
3275539@gmail.com <3275539@gmail.com>
2019-01-06 17:38:29 +0100

    Add project files.

---
+++ WebApplication1/Pages/About.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace WebApplication1.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            string directory = Environment.CurrentDirectory;
+            Message = String.Format("Your directory is {0}.", directory);
+        }
+    }
+}
